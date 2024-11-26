6d12ec9fa5debaef8b4aa82ef84fa4c93b40cb8e
Tomas Pica <tomas.picadelcanto@grafana.com>
2024-11-26 17:24:54 +0100

    fork init

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:generate mdatagen metadata.yaml
+
+// Package otlphttpexporter exports data by using the OTLP format to an HTTP endpoint.
+package otlphttpexporter // import "github.com/t00mas/otlphttpexporter"
